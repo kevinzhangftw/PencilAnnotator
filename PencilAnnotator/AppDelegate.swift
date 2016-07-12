e355e5f32f4c1a162ece62bdc92bc2c2aa872d3e
Kevin Zhang <kevinctzhang@gmail.com>
2016-07-11 23:19:31 -0700

    initial commit, memory leak detected

---
+++ PencilAnnotator/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  PencilAnnotator
+//
+//  Created by Kevin Zhang on 2016-07-01.
+//  Copyright © 2016 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
